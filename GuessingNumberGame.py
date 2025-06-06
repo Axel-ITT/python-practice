66e03e185a7c104c06dc8726798aacdd0a9a7895
Axel M. <209078823+Axel-ITT@users.noreply.github.com>
2025-06-06 14:51:27 +0200

    Group 3 initial

---
+++ GuessingNumberGame.py
@@ -0,0 +1,23 @@
+import random
+
+
+def getInput():
+    try:
+        return int(input("Guess a number from 1 to 5: "))
+    except ValueError:
+        print("Sorry, that's not a number. You Failed.")
+
+def check(guess):
+    secret_number = random.randint(1, 5)
+    if guess < secret_number:
+        print("Too low!")
+    elif guess > secret_number:
+        print("Too high!")
+    else:
+        print("You win!")
+
+while True:
+    guess = getInput()
+    if guess is not None:
+        break
+check(guess)
